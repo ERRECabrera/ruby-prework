e33901d3d74198c108c448c44c4e5e485d2c6610
ERRECabrera <juanparanadie@gmail.com>
2015-10-07 04:09:02 +0200

    Ajustes y arreglos en el Caesars-Cypher

--- caesars-cypher/caesars-cypher1.rb
+++ caesars-cypher/caesars-cypher1.rb
@@ -1,11 +1,11 @@
-#require "pry"
+require "pry"
 #encoding: utf-8
 
 def descifrado(mensaje, clav)
 	abc = "a".upto("z").to_a
 	result = []
-	mensaje.downcase
-	mensaje.each_char do |c|
+	minus = mensaje.downcase
+	minus.each_char do |c|
 		if c =~ /\W/
 			result.push c
 		else
@@ -22,8 +22,8 @@ def descifrado(mensaje, clav)
 				new_index = indice + clav.to_i
 				if new_index < 0
 					result.push abc.at(new_index)
-				elsif new_index > abc.length
-					renew_index = new_index - 26
+				elsif new_index > (abc.length - 1)
+					renew_index = 26 - new_index
 					result.push abc.at(renew_index)
 				else
 					result.push abc.at(new_index)
@@ -39,9 +39,15 @@ puts "Escriba el mensaje a descifrar:"
 mensaje_cifrado = gets.chomp
 puts "Indique la clave numérica:\n(-3, por defecto)"
 clave = gets.chomp
+
 until clave =~ /[-+]\d{1,}/ || clave == ""
 	puts "Por favor, indique la clave numérica con el formato + ó - seguido de un número:\nSi la deja en blanco será -3 por defecto"
 	clave = gets.chomp
 end
+until clave.to_i <= 26 && clave.to_i >= -26 
+	puts "Tenemos un límite de +-26.\nIntroduce otro valor."
+	clave = gets.chomp
+end
+
 puts descifrado(mensaje_cifrado,clave).capitalize
 #binding.pry
--- caesars-cypher/caesars-cypher2.rb
+++ caesars-cypher/caesars-cypher2.rb
@@ -3,8 +3,8 @@ require "pry"
 
 def descifrado(mensaje, clav)
 	result = []
-	mensaje.downcase
-	mensaje.each_char do |c|
+	minus = mensaje.downcase
+	minus.each_char do |c|
 		if c =~ /\W/
 			result.push c
 		else
@@ -24,7 +24,8 @@ def descifrado(mensaje, clav)
 					renew_ref = 123 - (97 - new_ref)
 					result.push renew_ref.chr
 				elsif new_ref > 122
-					renew_ref = new_ref - 122
+					ciclos = clav.to_i / 26
+					renew_ref = new_ref - 26
 					result.push renew_ref.chr
 				else
 					result.push new_ref.chr
@@ -40,9 +41,15 @@ puts "Escriba el mensaje a descifrar:"
 mensaje_cifrado = gets.chomp
 puts "Indique la clave numérica:\n(-3, por defecto)"
 clave = gets.chomp
+
 until clave =~ /[-+]\d{1,}/ || clave == ""
 	puts "Por favor, indique la clave numérica con el formato + ó - seguido de un número:\nSi la deja en blanco será -3 por defecto"
 	clave = gets.chomp
 end
+until clave.to_i <= 26 && clave.to_i >= -26 
+	puts "Tenemos un límite de +-26.\nIntroduce otro valor."
+	clave = gets.chomp
+end
+
 puts descifrado(mensaje_cifrado,clave).capitalize
 #binding.pry
